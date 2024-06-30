6035d3e88fc324ffdd9ea36adde50181d4dc4a6b
Johannes Kaufmann <johannes@joina.de>
2024-06-30 14:58:05 +0200

    added go mod

---
+++ dom.go
@@ -0,0 +1,5 @@
+// dom makes it easier to interact with the html document.
+//
+// Node = return all the nodes
+// Element = return all the nodes that are of type Element. This e.g. excludes #text nodes.
+package dom
